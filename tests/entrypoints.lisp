9ff6ac1979bfbb9ddcc646cfc2138f8048353e5c
TwigWM contributors <30662+digash@users.noreply.github.com>
2026-09-16 12:12:43 -0400

    Resolve extensionless executable paths in the entrypoint test

--- tests/entrypoints.lisp
+++ tests/entrypoints.lisp
@@ -1,6 +1,6 @@
 ;;;; Read and compile both executables without running installers or event taps.
 (dolist (file '("../bin/install" "../bin/twigwm"))
-  (with-open-file (s (merge-pathnames file *load-truename*))
+  (with-open-file (s (merge-pathnames file (uiop:pathname-directory-pathname *load-truename*)))
     (read-line s) ; interpreter line
     (loop for form = (read s nil :end) until (eq form :end) do
       (multiple-value-bind (function warnings failure)
